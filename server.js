a46a05e07588e3aa144dc3a7815eaec4e3d4bc36
Rafaela Johnson <rafaelajohnson1@gmail.com>
2025-08-11 22:04:33 -0400

    tweaked server js

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-import app from "#app";
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+
+const app = require("./app");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
